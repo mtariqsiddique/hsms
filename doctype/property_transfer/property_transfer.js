68e388640754d4f056601dfbd148c25c6bff190c
Tariq Siddique <tariq.siddique84@gmail.com>
2024-01-30 17:50:25 +0500

    remove doctype

--- doctype/property_transfer/property_transfer.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Tariq Siddique and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Property Transfer", {
-// 	refresh(frm) {
-
-// 	},
-// });
