0f399adcaac48ca2ee2aed5faedb0f12d6d6746f
Tariq Siddique <tariq.siddique84@gmail.com>
2024-10-27 09:56:07 -0400

    update revenue report

--- hsms/hsms/report/revenue_report/revenue_report.py
+++ hsms/hsms/report/revenue_report/revenue_report.py
@@ -1,19 +1,17 @@
-# Copyright (c) 2024, Tariq Siddique and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.utils import getdate
 
 def execute(filters=None):
     columns = [
         {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 120},
+        {"label": "Rept. No.", "fieldname": "receipt_number", "fieldtype": "Data", "width": 80},
         {"label": "Document Type", "fieldname": "doctype", "fieldtype": "Data", "width": 200}, 
         {"label": "Document Name", "fieldname": "name", "fieldtype": "Dynamic Link", "options": "doctype", "width": 150},
-        {"label": "Customer", "fieldname": "customer_code", "fieldtype": "Link", "options": "Customer","width": 150},
-        {"label": "Customer Name", "fieldname": "customer_name", "fieldtype": "Data", "width": 300},
-        {"label": "Property Number", "fieldname": "property_number", "fieldtype": "Link", "options": "Inventory Master Data", "width": 120},
-        {"label": "Transaction Type", "fieldname": "transaction_type", "fieldtype": "Data", "width": 250},
-        {"label": "Net Amount", "fieldname": "net_amount", "fieldtype": "Currency", "width": 150},		
+        {"label": "Customer", "fieldname": "customer_code", "fieldtype": "Link", "options": "Customer", "width": 120},
+        {"label": "Customer Name", "fieldname": "customer_name", "fieldtype": "Data", "width": 250},
+        {"label": "Property No.", "fieldname": "property_number", "fieldtype": "Link", "options": "Inventory Master Data", "width": 80},
+        {"label": "Transaction Type", "fieldname": "transaction_type", "fieldtype": "Data", "width": 200},
+        {"label": "Net Amount", "fieldname": "net_amount", "fieldtype": "Currency", "width": 150},
     ]
 
     from_date = getdate(filters.get("from_date"))
@@ -24,18 +22,24 @@ def execute(filters=None):
 
     def fetch_records(doc_type, child_table, fields, doctype_name_field, parent_customer_field, parent_customer_code, filter_field="posting_date"):
         records = []
+        parent_fields = ["name", "posting_date", parent_customer_field, parent_customer_code, "property_number"]
+        
+        if frappe.get_meta(doc_type).has_field("receipt_number"):
+            parent_fields.append("receipt_number")
+
         conditions = [
             [doc_type, "docstatus", "=", 1],
             [doc_type, filter_field, ">=", from_date],
             [doc_type, filter_field, "<=", to_date]
         ]
-        parent_docs = frappe.get_all(doc_type, filters=conditions, fields=["name", "posting_date", parent_customer_field, parent_customer_code, "property_number"])
+        parent_docs = frappe.get_all(doc_type, filters=conditions, fields=parent_fields)
 
         for parent in parent_docs:
             child_docs = frappe.get_all(child_table, filters={"parent": parent.name}, fields=fields)
             for child in child_docs:
                 record = {
                     "posting_date": parent.posting_date,
+                    "receipt_number": parent.get("receipt_number"),  
                     "customer_name": parent.get(parent_customer_field),
                     "customer_code": parent.get(parent_customer_code),
                     "property_number": parent.property_number,
